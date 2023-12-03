36274ee6bf8728087866df9b648d1b220ca81fcc
LaupWing <loc_strict@hotmail.com>
2023-12-03 16:31:14 +0100

    sensing image

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "react"
 import express from "express"
 import ReactDOMServer from "react-dom/server"
+import puppeteer from "puppeteer"
 
 const app = express()
 const port = 3000
@@ -14,8 +15,26 @@ const ImageComponent = () => {
    `
 }
 
-app.get("/", (req, res) => {
-   res.send("Hello World!")
+app.get("/test", async (req, res) => {
+   const browser = await puppeteer.launch({
+      headless: "new",
+   })
+   const page = await browser.newPage()
+
+   // Get the HTML string from the ImageComponent
+   const html = ImageComponent()
+
+   // Set the HTML content on the page
+   await page.setContent(html)
+
+   // Capture a screenshot of the rendered content
+   const screenshot = await page.screenshot()
+
+   // Send the image as the response
+   res.type("image/png").send(screenshot)
+
+   // Close the browser
+   await browser.close()
 })
 
 app.listen(port, () => {
