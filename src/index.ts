bdb2763fdd03a6808a7b4eea5dd984a2359f79c9
LaupWing <loc_strict@hotmail.com>
2023-12-03 19:07:31 +0100

    this cannot be done unfrotanely

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express"
 import path from "path"
 import puppeteer from "puppeteer"
+import postcss from "postcss"
+import tailwindcss from "tailwindcss"
 
 const app = express()
 
@@ -16,11 +18,14 @@ async function renderTemplate(viewName: string, data: any) {
    })
 }
 
+const inputFilePath = './src/styles/tailwind.css';
+const outputFilePath = './dist/output.css';
+
 app.set("view engine", "ejs")
    .use(express.static(path.join(process.cwd(), "public")))
    .set("views", path.join(process.cwd(), "views"))
    .get("/", async (req, res) => {
-      const browser = await puppeteer.launch({ 
+      const browser = await puppeteer.launch({
          headless: false,
       })
       console.log(path.join(__dirname, "views"))
@@ -31,7 +36,9 @@ app.set("view engine", "ejs")
       // res.send('test')
       // Set the HTML content on the page
       // res.send("hi")
-      await page.setContent(renderedHTML as string, { waitUntil: "domcontentloaded" })
+      await page.setContent(renderedHTML as string, {
+         waitUntil: "domcontentloaded",
+      })
 
       // // Capture a screenshot of the rendered content
       const screenshot = await page.screenshot()
@@ -40,6 +47,17 @@ app.set("view engine", "ejs")
       res.type("image/png").send(screenshot)
 
       await browser.close()
+   })
+   .get("/test", async (req, res) => {
+      const renderedHTML = await renderTemplate("index", { name: "John Doe" })
+      const result = await postcss([
+         tailwindcss,
+      ]).process(renderedHTML as string, {
+         from: inputFilePath,
+         to: outputFilePath,
+      })
+
+      console.log(result.css)
    })
 
 app.listen(3000, () => console.log("Server is running on port 3000"))
