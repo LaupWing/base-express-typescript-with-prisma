1d2d21b1329c6f4f5abecaa322cf13e4be0a3d82
LaupWing <loc_strict@hotmail.com>
2023-12-03 18:46:02 +0100

    udpate

--- src/index.ts
+++ src/index.ts
@@ -4,30 +4,41 @@ import puppeteer from "puppeteer"
 
 const app = express()
 
-app
-   .set("view engine", "ejs")
+async function renderTemplate(viewName: string, data: any) {
+   return new Promise((resolve, reject) => {
+      app.render(viewName, data, (err, html) => {
+         if (err) {
+            reject(err)
+         } else {
+            resolve(html)
+         }
+      })
+   })
+}
+
+app.set("view engine", "ejs")
    .use(express.static(path.join(process.cwd(), "public")))
    .set("views", path.join(process.cwd(), "views"))
    .get("/", async (req, res) => {
       const browser = await puppeteer.launch()
       console.log(path.join(__dirname, "views"))
       const page = await browser.newPage()
-      return res.render("index", {
-         title: "Hello, OpenGraph Image!"
-      })
+      const renderedHTML = await renderTemplate("index", { name: "John Doe" })
+      await page.addStyleTag({url: 'http://example.com/style.css'})
+      // res.send(renderedHTML)
       // console.log(renderedHTML)
-      // // res.send('test')
-      // // Set the HTML content on the page
-      // await page.setContent(renderedHTML!, { waitUntil: "domcontentloaded" })
+      // res.send('test')
+      // Set the HTML content on the page
+      // res.send("hi")
+      await page.setContent(renderedHTML as string, { waitUntil: "domcontentloaded" })
 
       // // Capture a screenshot of the rendered content
-      // const screenshot = await page.screenshot()
+      const screenshot = await page.screenshot()
 
-      // // Send the image as the response
-      // res.type("image/png").send(screenshot)
+      // Send the image as the response
+      res.type("image/png").send(screenshot)
 
-      // await browser.close()
+      await browser.close()
    })
-
 
 app.listen(3000, () => console.log("Server is running on port 3000"))
