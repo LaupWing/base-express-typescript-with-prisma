6d215b54c49463c78a4337dbcda452bc024dc14a
LaupWing <loc_strict@hotmail.com>
2023-12-03 16:27:37 +0100

    removed for now

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,17 @@ import "react"
 import express from "express"
 import ReactDOMServer from "react-dom/server"
 
-
 const app = express()
 const port = 3000
+
+const ImageComponent = () => {
+   return `
+      <div className="bg-blue-500 text-white p-20 font-sans">
+         <h1>Hello, Dynamic Image!</h1>
+         {/* Add more JSX as needed */}
+      </div>
+   `
+}
 
 app.get("/", (req, res) => {
    res.send("Hello World!")
