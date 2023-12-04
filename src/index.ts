f21117d6c4bf3dad232d28422c4e4f28f3e804a5
LaupWing <loc_strict@hotmail.com>
2023-12-04 09:12:29 +0100

    moved some stuff

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import express from "express"
 import path from "path"
-import puppeteer from "puppeteer"
 import postcss from "postcss"
 import tailwindcss from "tailwindcss"
 
 const app = express()
 
-async function renderTemplate(viewName: string, data: any) {
+export async function renderTemplate(viewName: string, data: any) {
    return new Promise((resolve, reject) => {
       app.render(viewName, data, (err, html) => {
          if (err) {
@@ -25,28 +24,7 @@ app.set("view engine", "ejs")
    .use(express.static(path.join(process.cwd(), "public")))
    .set("views", path.join(process.cwd(), "views"))
    .get("/", async (req, res) => {
-      const browser = await puppeteer.launch({
-         headless: false,
-      })
-      console.log(path.join(__dirname, "views"))
-      const page = await browser.newPage()
-      const renderedHTML = await renderTemplate("index", { name: "John Doe" })
-      // res.send(renderedHTML)
-      // console.log(renderedHTML)
-      // res.send('test')
-      // Set the HTML content on the page
-      // res.send("hi")
-      await page.setContent(renderedHTML as string, {
-         waitUntil: "domcontentloaded",
-      })
-
-      // // Capture a screenshot of the rendered content
-      const screenshot = await page.screenshot()
-
-      // Send the image as the response
-      res.type("image/png").send(screenshot)
-
-      await browser.close()
+      
    })
    .get("/test", async (req, res) => {
       const renderedHTML = await renderTemplate("index", { name: "John Doe" })
--- src/routes/og.ts
+++ src/routes/og.ts
@@ -1,9 +1,32 @@
 import express from "express"
-
+import puppeteer from "puppeteer"
+import path from "path"
+import { renderTemplate } from ".."
 const router = express.Router()
 
-router.get("/", (req, res) => {
-   res.send("hello")
+router.get("/", async (req, res) => {
+   const browser = await puppeteer.launch({
+      headless: false,
+   })
+   console.log(path.join(__dirname, "views"))
+   const page = await browser.newPage()
+   const renderedHTML = await renderTemplate("index", { name: "John Doe" })
+   // res.send(renderedHTML)
+   // console.log(renderedHTML)
+   // res.send('test')
+   // Set the HTML content on the page
+   // res.send("hi")
+   await page.setContent(renderedHTML as string, {
+      waitUntil: "domcontentloaded",
+   })
+
+   // // Capture a screenshot of the rendered content
+   const screenshot = await page.screenshot()
+
+   // Send the image as the response
+   res.type("image/png").send(screenshot)
+
+   await browser.close()
 })
 
 export default router
